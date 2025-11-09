cb1ce004592ded204af116b1921aa2615f007b1b
akshay7745 <akshay7745@gmail.com>
2025-11-09 13:48:11 +0530

    create user schema

---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  isMarried: {
+    type: Boolean,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
