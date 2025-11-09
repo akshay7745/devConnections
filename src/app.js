d004a1edba9c40fe4441c55f29bfa998259010bc
akshay7745 <akshay7745@gmail.com>
2025-11-09 17:17:30 +0530

    save user data to database dynamically or add json middlerware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,38 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database.js");
+const User = require("./models/user.js");
+
+// The express.json() middleware is used to parse JSON bodies, means the incoming data from request body our server cant understand or read it directly so we need to use this middleware to parse it and make it readable for our server(this middleware is reads the json data passed from request body and converts it into javascript object and attaches it to req.body)
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  // const user = new User({
+  //   firstName: "Govind",
+  //   lastName: "Hari",
+  //   email: "govind@golok.hare",
+  //   password: "HareKrishnaMahamantra",
+  //   age: 16,
+  //   gender: "purush",
+  //   address: "Golok Vrindavan",
+  //   isMarried: true,
+  // });
+  // user.save method returns a promise so it is better to use async await or then catch
+  // for now we will convert this function into async
+
+  const user = new User(req.body);
+
+  try {
+    const userData = await user.save();
+    res
+      .status(201)
+      .json({ message: "User signed up successfully", user: userData });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Error signing up user");
+  }
+});
 
 connectDB()
   .then((res) => {
