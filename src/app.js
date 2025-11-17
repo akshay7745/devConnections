fa0eedf1b9acce3999b21ce9f9c27faba17d9266
akshay7745 <akshay7745@gmail.com>
2025-11-17 09:07:30 +0530

    add auth middleware to secure api

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ const User = require("./models/user.js");
 const bcrypt = require("bcrypt");
 const { validateSignupData } = require("./utils/signupValidation.js");
 const validator = require("validator");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 // The express.json() middleware is used to parse JSON bodies, means the incoming data from request body our server cant understand or read it directly so we need to use this middleware to parse it and make it readable for our server(this middleware is reads the json data passed from request body and converts it into javascript object and attaches it to req.body)
+const { userAuth } = require("./middlewares/auth.js");
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.post("/signup", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
@@ -46,75 +49,28 @@ app.post("/login", async (req, res) => {
       throw new Error("Invalid credentials");
     }
 
-    res.status(200).json({ message: "Login successful", user });
+    const token = jwt.sign({ _id: user._id }, "HariBol", {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    });
+
+    res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     res.status(500).send("Error logging in user: " + error.message);
   }
 });
-app.post("/updateUser/:id", async (req, res) => {
-  const { id } = req.params;
 
-  const allowedValues = [
-    "firstName",
-    "lastName",
-    "age",
-    "gender",
-    "address",
-    "skills",
-    "isMarried",
-    "photoUrl",
-  ];
-
-  const allowUpdate = Object.keys(req.body).every((key) => {
-    return allowedValues.includes(key);
-  });
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    if (!allowUpdate) {
-      return res.status(400).send("Invalid updates!");
-    }
-
-    if (req.body?.skills.length > 10) {
-      return res.status(400).send("Skills cannot be more than 10");
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
-      runValidators: true,
-      returnDocument: "after",
-    });
-
-    res.status(200).json({
-      message: "User updated successfully",
-      user: updatedUser,
-    });
+    res
+      .status(200)
+      .json({ message: "Cookie read successfully", user: req.user });
   } catch (error) {
-    res.status(500).send("Error updating user data " + error.message);
-  }
-});
-
-app.get("/userOne", async (req, res) => {
-  const userEmail = req.body.email;
-  try {
-    const user = await User.findOne({ email: userEmail });
-    if (!user) {
-      return res.status(404).send("User not found");
-    } else {
-      res.status(200).json({ user: user });
-    }
-  } catch (error) {
-    res.status(500).send("Error fetching user data");
-  }
-});
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.status(404).send("No users found");
-    } else {
-      res.status(200).json({ users });
-    }
-  } catch (error) {
-    res.status(500).send("Error fetching users data");
+    res.status(500).send("Error reading cookie " + error.message);
   }
 });
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 const adminAuth = (req, res, next) => {
   const token = "lslsiosos";
 
@@ -9,13 +11,29 @@ const adminAuth = (req, res, next) => {
   }
 };
 
-const userAuth = (req, res, next) => {
-  const userToken = "userxlslsls";
-  if (userToken === "userlslsls") {
-    console.log("User authenticated");
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("No token found");
+    }
+
+    const decodedData = jwt.verify(token, "HariBol");
+
+    const { _id } = decodedData;
+    if (!_id) {
+      throw new Error("User not authenticated");
+    }
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
     next();
-  } else {
-    res.status(403).send("User not authenticated");
+  } catch (error) {
+    res.status(401).send("User authentication failed " + error.message);
   }
 };
 
